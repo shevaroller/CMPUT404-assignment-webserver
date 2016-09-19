2514c19c2d636aaffd3f0a1ca3e82f045dddeed2
Alex Shevchenko <shevaroller@gmail.com>
2016-09-19 15:56:58 -0600

    remove print statements for cleaner checking with script

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
+        #print ("Got a request of: %s\n" % self.data)
 
         header = self.data.split("\n")
         request = header[0].split(" ")
@@ -41,11 +41,11 @@ class MyWebServer(SocketServer.BaseRequestHandler):
              url = request[1]
              self.getUrl(url)
         else:
-             print "Error\nGET is expected instead of " + request[0]
+             #print "Error\nGET is expected instead of " + request[0]
              self.getUrl("404")
 
     def getUrl(self, url):
-        print "Get this url: " + url
+        #print "Get this url: " + url
 
         # construct path to requested file
         path = "www"
@@ -54,14 +54,14 @@ class MyWebServer(SocketServer.BaseRequestHandler):
             if url[-1] == "/":
                 path += "index.html"
             else:
-                print "Respond with 302 Moved Temporarily\n\n"
-                header = "HTTP/1.1 302 Moved Temporarily\n"
-                header += "Location: " + url + "/\n"
+                #print "Respond with 302 Moved Temporarily\n\n"
+                header = "HTTP/1.1 302 Moved Temporarily\r\n"
+                header += "Location: " + url + "/\r\n"
                 header += "Connection: Closed\r\n\r\n"
                 data = ''
                 self.sendResponse(header, data)
 
-        print "Trying to serve " + path
+        #print "Trying to serve " + path
         try:
             f = open(path, 'r')
             absolutePath = os.path.abspath(f.name)
@@ -72,16 +72,16 @@ class MyWebServer(SocketServer.BaseRequestHandler):
                 raise FileNotFoundError("You don't have permissions to access file " + f.name)
             data = f.read()
             # consulted with http://www.tutorialspoint.com/http/http_responses.htm
-            header = "HTTP/1.1 200 OK\n"
+            header = "HTTP/1.1 200 OK\r\n"
             mime = path.split(".")[-1]
-            print "Respond with 200 OK\n\n"
+            #print "Respond with 200 OK\n\n"
         except:
-            header = "HTTP/1.1 404 Not Found\n"
+            header = "HTTP/1.1 404 Not Found\r\n"
             data = "<h1>404 Not Found</h1>"
             mime = "html"
-            print "Respond with 404 Not Found\n\n"
-        header += "Content-Length: " + str(len(data)) + "\n"
-        header += "Content-Type: text/" + mime + "\n"
+            #print "Respond with 404 Not Found\n\n"
+        header += "Content-Length: " + str(len(data)) + "\r\n"
+        header += "Content-Type: text/" + mime + "\r\n"
         header += "Connection: Closed\r\n\r\n"
         self.sendResponse(header, data)
 
